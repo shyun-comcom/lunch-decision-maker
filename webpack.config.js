972ae40fa7e668b961277caaa3fd8a37a14b958d
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-04-20 20:47:03 +0900

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   entry: ['babel-polyfill', './src/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [{
@@ -33,6 +34,8 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   devServer: {
+    contentBase: path.join(__dirname, outputDirectory),
+    historyApiFallback: true,
     port: 3000,
     open: true
   },
