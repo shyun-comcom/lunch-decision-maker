0cb405a0b9f5a97c468432069d014bbcb127d242
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-05-18 15:26:47 +0900

    fix geoloc error page UI

--- src/client/pages/Error.js
+++ src/client/pages/Error.js
@@ -10,17 +10,21 @@ export default class ErrorPage extends Component {
 
   render = () => {
     return (
-        <div className="app-root-div" style={{padding: '60px 40px 60px 40px'}}>
-            <div style={{fontSize: '20px', color: '#929292', 
-                    fontFamily: 'Noto Sans KR', fontWeight: 'bold'}}>
-                <div>위치 정보 서비스 공유를</div>
-                <div>허용해 주세요</div>
+        <div className="app-root-div">
+          <div className="app-page-wrapper">
+            <div className="app-main-div">
+              <div style={{fontSize: '20px', color: '#929292', width: '280px',
+                      fontFamily: 'Noto Sans KR', fontWeight: 'bold'}}>
+                  <div>위치 정보 서비스 공유를</div>
+                  <div>허용해 주세요</div>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'column',
+                  alignItems: 'center', justifyContent: 'center',
+                  paddingTop: '136px', paddingBottom: '136px'}}>
+                  <img src={WorldMap} width={160} height={160} />
+              </div>
             </div>
-            <div style={{display: 'flex', flexDirection: 'column',
-                alignItems: 'center', justifyContent: 'center',
-                paddingTop: '136px', paddingBottom: '136px'}}>
-                <img src={WorldMap} width={160} height={160} />
-            </div>
+          </div>
         </div>
     )
   }
